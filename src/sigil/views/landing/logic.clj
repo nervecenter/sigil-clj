a077e36db9502c1536853e739eaf08b6406e7be5
Chris <nervecenter@outlook.com>
2016-02-09 14:20:42 -0500

    Starting working on a temporary landing page to test resource directories. No routes yet.

---
+++ src/sigil/views/landing/logic.clj
@@ -0,0 +1,10 @@
+(ns sigil.views.landing.logic
+  (:require '[hiccup.core :as h]))
+
+(def page
+  (h/html
+   [:head
+    [:title "Sigil"]]
+   [:body.page
+    [:div.page-header.page-header-landing
+     [:img {:src "/public/sample.png"}]]]))
