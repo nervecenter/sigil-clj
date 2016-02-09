3bf24c68a8f006752f8dcd4dbdb4edbb881ea706
Chris <nervecenter@outlook.com>
2016-02-09 17:01:04 -0500

    Uploaded resources. Working on landing page.

--- src/sigil/views/landing/logic.clj
+++ src/sigil/views/landing/logic.clj
@@ -1,9 +1,24 @@
 (ns sigil.views.landing.logic
-  (:require [hiccup.core :as h]))
+  (:use hiccup.core
+        hiccup.page))
 
 (def page
-  (h/html
+  (html5
    [:head
+    [:meta {:charset "utf-8"}]
+    [:meta {:name "viewport" :content "width=device-width, initial-scale=1.0"}]
+
+    [:link {:rel "shortcut icon" :href "images/favicon.png"}]
+
+    (include-js "js/jquery-1.11.3.js"
+                "js/jquery-ui-1.9.2.custom.min.js")
+
+    (include-css "css/jquery-ui-1.9.2.custom.css"
+                 "css/bootstrap-flatly.css"
+                 "css/site.css")
+
+
+
     [:title "Sigil"]]
    [:body.page
     [:div.page-header.page-header-landing
