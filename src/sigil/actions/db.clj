6fe3b8bcc03e0cdca0d409d8c4254a217b1e27a4
Dominic <dominicacox@gmail.com>
2016-03-17 14:49:49 -0400

    Forgot to grab return in new-issue-post. Project builds and runs.

--- src/sigil/actions/db.clj
+++ src/sigil/actions/db.clj
@@ -66,6 +66,7 @@
 (defn add-issue-post
   [req]
   (let [new-issue-data (:form-params req)
+        return (new-issue-data "return")
         new-issue (zipmap [:org_id :user_id :title :text]
                           (map #(new-issue-data %) ["org-id" "user-id" "title" "text"]))]
     (if (= :success (db/db-trans [issues/create-issue new-issue]))
